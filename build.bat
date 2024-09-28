22691639dd2c0fcb67e525d1c6b7304f631f70df
Gianmaria <gianmaria.bonici@gmail.com>
2024-09-28 12:25:19 +0200

    added example

---
+++ build.bat
@@ -0,0 +1,5 @@
+cl /D _CRT_SECURE_NO_WARNINGS ^
+/I . /external:anglebrackets /external:W0 ^
+/EHsc ^
+/wd5045 /wd4820 /Wall ^
+/std:c++17 main.cpp
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#define FLOGGER_IMPLEMENTATION
+#include "flogger.h"
+
+int main()
+{
+    Flog("hello world");
+
+    return 0;
+}
