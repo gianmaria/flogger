34515a84783da56ec0bf8a8521dce70cfb53cc35
john <otakon89@gmail.com>
2025-03-21 17:15:32 +0100

    Update flogger.h

--- flogger.h
+++ flogger.h
@@ -1,35 +1,32 @@
 #ifndef FLOGGER_H
 #define FLOGGER_H
 
+#include "format.h"
 #include <chrono>
 #include <cstring> // strrchr
 #include <ctime>
-//#include <format>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <mutex>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <thread>
 #include <utility>
-#include <string_view>
 
-#define FMT_HEADER_ONLY 1
-#include <fmt/format.h>
 
 class Flogger
 {
 public:
-    
     enum Opt : uint16_t
     {
-        none     = 0x0,
-        time     = 0x1,
-        tid      = 0x2,
-        file     = 0x4,
+        none = 0x0,
+        time = 0x1,
+        tid = 0x2,
+        file = 0x4,
         function = 0x8,
-        all      = time | tid | file | function
+        all = time | tid | file | function
     };
 
     Flogger(const Flogger&) = delete;
@@ -45,9 +42,14 @@ public:
     }
 
     template<typename... Args>
-    void log(uint16_t options, std::string_view time, std::string_view tid, 
-             const char* file_, int line, const char* function,
-             std::string_view fmt, Args&&... args)
+    void log(uint16_t options,
+             std::string_view time,
+             std::string_view tid,
+             const char* file_,
+             int line,
+             const char* function,
+             std::string_view fmt,
+             Args&&... args)
     {
         std::lock_guard<std::mutex> lock(mtx); // Lock the mutex to ensure thread safety
 
@@ -59,10 +61,10 @@ public:
         int separator = '/';
 #endif
         const char* file = (strrchr(file_, separator) ? strrchr(file_, separator) + 1 : file_);
-        
+
         if ((options & Opt::time) == Opt::time)
             buffer << time << "|";
-        
+
         if ((options & Opt::tid) == Opt::tid)
             buffer << tid << "|";
 
@@ -74,15 +76,29 @@ public:
 
         if (options != Opt::none)
             buffer << " ";
-        
+
         buffer << msg << "\n";
 
-        /*buffer << time << "|" << tid << "|"
-            << file << ":" << line << "|"
-            << "@" << function << "| "
-            << msg << "\n";*/
-
         flush_Buffer();
+    }
+
+
+    static std::string getCurrentDate()
+    {
+        // Get current time
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+
+        // Convert to tm struct
+        std::tm* local_tm = std::localtime(&now_time);
+
+        // Format the date string
+        std::ostringstream oss;
+        oss << std::setfill('0') << std::setw(2) << local_tm->tm_mday << "/" // day
+            << std::setw(2) << (local_tm->tm_mon + 1) << "/"                 // month (Jan is 0)
+            << (local_tm->tm_year + 1900);                                   // year (years since 1900)
+
+        return oss.str();
     }
 
     static std::string get_Current_Time()
@@ -93,8 +109,7 @@ public:
         std::tm* local_time = std::localtime(&now_time);
 
         std::ostringstream oss;
-        oss << std::put_time(local_time, "%H:%M:%S") 
-            << '.' << std::setw(3) << std::setfill('0') << ms.count();
+        oss << std::put_time(local_time, "%H:%M:%S") << '.' << std::setw(3) << std::setfill('0') << ms.count();
         return oss.str();
     }
 
@@ -104,11 +119,7 @@ public:
 
         // Convert the thread ID to a numeric representation using a stringstream
         std::ostringstream oss;
-        oss << std::hex 
-            << std::setw(8) 
-            << std::setfill('0') 
-            << std::uppercase 
-            << this_id;
+        oss << std::hex << std::setw(8) << std::setfill('0') << std::uppercase << this_id;
 
         std::string thread_id_str = "0x";
         thread_id_str.append(oss.str());
@@ -123,15 +134,18 @@ public:
     }
 
 protected:
-
-    Flogger(std::string_view log_file_path) 
-        : log_file_path(log_file_path)
+    Flogger(std::string_view log_file_path) : log_file_path(log_file_path)
     {
         out_file.open(log_file_path.data(), std::ios::binary | std::ios::app);
         if (!out_file.is_open())
         {
             std::cerr << "Error: Unable to open log file: " << log_file_path << std::endl;
+            return;
         }
+
+        buffer << "\n\n\n\n\n\n\n\n\n\n";
+        buffer << "========================== NEW RUN " << getCurrentDate() << " ==========================\n";
+        flush_Buffer();
     }
 
 private:
@@ -139,28 +153,45 @@ private:
     std::mutex mtx;                  // mutex to protect file access
     std::ostringstream buffer;       // buffer to accumulate log messages
     const std::string log_file_path; // default log file path
-    uint16_t options;
+    // uint16_t options;
 
-    // Flush the buffer to the file with a timestamp
     void flush_Buffer()
     {
-        if (!buffer.str().empty() && 
-            out_file.is_open())
+        if (not buffer.str().empty() && out_file.is_open())
         {
             out_file << buffer.str();
             out_file.flush(); // flush to disk at every log operation
 
-            buffer.str("");  // Clear the buffer
-            buffer.clear();  // Clear any error state
+            buffer.str(""); // Clear the buffer
+            buffer.clear(); // Clear any error state
         }
     }
 };
 
 extern Flogger& default_flog;
 
-#define Flog(fmt, ...)  default_flog.log(Flogger::all,  Flogger::get_Current_Time(), Flogger::get_TID(), __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
-#define Flogb(fmt, ...) default_flog.log(Flogger::none, Flogger::get_Current_Time(), Flogger::get_TID(), __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
-#define Ftrace          default_flog.log(Flogger::file, Flogger::get_Current_Time(), Flogger::get_TID(), __FILE__, __LINE__, __func__, "{}", "*trace*")
+#define Flog(fmt, ...)                              \
+    default_flog.log(Flogger::time | Flogger::file, \
+                     Flogger::get_Current_Time(),   \
+                     Flogger::get_TID(),            \
+                     __FILE__,                      \
+                     __LINE__,                      \
+                     __func__,                      \
+                     fmt,                           \
+                     ##__VA_ARGS__)
+
+#define Flogb(fmt, ...)                           \
+    default_flog.log(Flogger::none,               \
+                     Flogger::get_Current_Time(), \
+                     Flogger::get_TID(),          \
+                     __FILE__,                    \
+                     __LINE__,                    \
+                     __func__,                    \
+                     fmt,                         \
+                     ##__VA_ARGS__)
+#define Ftrace        \
+    default_flog.log( \
+      Flogger::file, Flogger::get_Current_Time(), Flogger::get_TID(), __FILE__, __LINE__, __func__, "{}", "*trace*")
 
 #endif // FLOGGER_H
 
